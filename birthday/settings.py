cfc92d0556e5d5bff5ed8e13168b781b89f03ce3
YKS-11 <yogeshkrishnan.s@ionixxtech.com>
2022-08-21 17:10:20 +0530

    update for memories section

--- birthday/settings.py
+++ birthday/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-)5l9rvu!@fu7&)zt^-**blzko9cp5p%t($8)6ypwa9q=!n)2#s'
+SECRET_KEY = 'django-insecure-)5l9rvu!@fu7&)zt^-**blzko9cp5p%t($8)6ypwa9q=!n)2#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,4 +136,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
